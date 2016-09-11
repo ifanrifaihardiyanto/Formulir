208a37b894032689397fa2696373e0968670d073
Ifan Rifa'i Hardiyanto <ifanrifai24@gmail.com>
2016-09-11 14:01:03 +0700

    Membuat inisialisasi pada Text View Tanggal Keberangkatan, Asal, dan Tujuan. membuat var dan inisialisasi RadioButton. membuat var dan inisialisasi CheckBox.

--- app/src/main/java/id/sch/smktelkom_mlg/learn/formulir/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/formulir/MainActivity.java
@@ -5,134 +5,100 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    /*EditText etTgl;
+    EditText etNm;
     EditText etAsal;
-    EditText etTujuan;*/
+    EditText etTujuan;
     Button bOK;
-    TextView tvHasil;
+    TextView tvHasil, tvkereta;
     RadioButton rbEks, rbBis, rbEk;
+    RadioGroup rgKelas;
     CheckBox cbDew, cbAn;
+    Spinner spK;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       /* etTgl = (EditText) findViewById(R.id.editTextTgl);
+        etNm = (EditText) findViewById(R.id.editTextNm);
         etAsal = (EditText) findViewById(R.id.editTextAsal);
-        etTujuan = (EditText) findViewById(R.id.editTextTujuan);*/
-//        bOK = (Button) findViewById(R.id.buttonOK);
+        etTujuan = (EditText) findViewById(R.id.editTextTujuan);
+        bOK = (Button) findViewById(R.id.buttonOK);
         rbEks = (RadioButton) findViewById(R.id.radioButtonEks);
         rbBis = (RadioButton) findViewById(R.id.radioButtonBis);
         rbEk = (RadioButton) findViewById(R.id.radioButtonEk);
-        /*etAsal.setText("");
-        etTgl.setText("");
-        etTujuan.setText("");*/
+        rgKelas = (RadioGroup) findViewById(R.id.radioGroupKelas);
         cbDew = (CheckBox) findViewById(R.id.checkBoxDew);
         cbAn = (CheckBox) findViewById(R.id.checkBoxAn);
+        cbDew.setOnCheckedChangeListener((CompoundButton.OnCheckedChangeListener) this);
+        cbAn.setOnCheckedChangeListener((CompoundButton.OnCheckedChangeListener) this);
+        spK = (Spinner) findViewById(R.id.spinnerKereta);
 
         tvHasil = (TextView) findViewById(R.id.textViewHasil);
+        tvkereta = (TextView) findViewById(R.id.textViewKrt);
 
-       /* bOK.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String tgl = etTgl.getText().toString();
-                String Asal = etAsal.getText().toString();
-                String Tujuan = etTujuan.getText().toString();
-                tvHasil.setText(tgl + "\n" + Asal + "\n" + Tujuan);
-                doProses();
-            }
 
-            private void doProses() {
-                if (isValid()) {
-                    String tgl = etTgl.getText().toString();
-                    String Asal = etAsal.getText().toString();
-                    String Tujuan = etTujuan.getText().toString();
-                    tvHasil.setText(tgl + "\n" + Asal + "\n" + Tujuan);
-                }
-            }
-
-            private boolean isValid() {
-                boolean valid = true;
-
-                String Tanggal = etTgl.getText().toString();
-                String  Asal = etAsal.getText().toString();
-                String  Tujuan = etTujuan.getText().toString();
-
-                if (Tanggal.isEmpty())
-                {
-                    etTgl.setError("Tidak Boleh Kosong");
-                    valid = false;
-                }
-                else
-                {
-                    etTgl.setError(null);
-                }
-
-                if (Asal.isEmpty())
-                {
-                    etAsal.setError("Tidak Boleh Kosong!");
-                    valid = false;
-                }
-                else
-                {
-                    etAsal.setError(null);
-                }
-
-                if (Tujuan.isEmpty())
-                {
-                    etTujuan.setError("Tidak Boleh Kosong!");
-                }
-                else
-                {
-                    etTujuan.setError(null);
-                }
-                return valid;
-            }
-        });*/
-
-        findViewById(R.id.buttonOK).setOnClickListener(new View.OnClickListener() {
+        bOK.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 doClick();
-                Click();
-            }
+        }
         });
-
     }
 
     private void doClick() {
+        String nama = etNm.getText().toString();
+        String Asal = etAsal.getText().toString();
+        String Tujuan = etTujuan.getText().toString();
         String hasil = null;
+        String NmK = spK.getSelectedItem().toString();
+        String Kereta = "\n Kereta";
 
-        if (rbEks.isChecked()) {
-            hasil = rbEks.getText().toString();
-        } else if (rbBis.isChecked()) {
-            hasil = rbBis.getText().toString();
-        } else if (rbEk.isChecked()) {
-            hasil = rbEk.getText().toString();
+        int startlen = Kereta.length();
+        if (cbDew.isChecked()) Kereta += cbDew.getText() + ",";
+        if (cbAn.isChecked()) Kereta += cbAn.getText() + ",";
+
+        if (Kereta.length() == startlen) Kereta = "Tidak Ada Pada Pilihan";
+
+        if (rgKelas.getCheckedRadioButtonId() != -1) {
+            RadioButton rb = (RadioButton) findViewById(rgKelas.getCheckedRadioButtonId());
+
+            hasil = rb.getText().toString();
         }
 
-        if (hasil == null) {
-            tvHasil.setText("Anda Belum Memilih");
+        if (nama.isEmpty()) {
+            etNm.setError("Nama Belum Diisi");
+        } else if (nama.length() < 20) {
+            etNm.setError("Nama Minimal 20 Karakter");
         } else {
-            tvHasil.setText("Kelas :" + hasil);
+            etNm.setError(null);
         }
 
-    }
+        if (Asal.isEmpty()) {
+            etAsal.setError("Belum Diisi");
+        } else if (Asal.length() < 4) {
+            etAsal.setError("Minimal 4 Karakter");
+        } else {
+            etAsal.setError(null);
+        }
 
-    private void Click() {
-        String hasil = "Usia:\n";
-        int startlen = hasil.length();
-        if (cbDew.isChecked()) hasil += cbDew.getText() + "\n";
-        if (cbAn.isChecked()) hasil += cbAn.getText() + "\n";
-
-        if (hasil.length() == startlen) hasil += "Tidak";
-
-        tvHasil.setText(hasil);
+        if (Tujuan.isEmpty()) {
+            etTujuan.setError("Belum Diisi");
+        } else if (Tujuan.length() < 4) {
+            etTujuan.setError("Minimal 4 Karakter");
+        } else {
+            etTujuan.setError(null);
+        }
+        tvHasil.setText("--------------- PEMBELIAN BERHASIL ---------------" + "\n Nama :" + nama + "\nAsal :" + Asal + "\nTujuan :" + Tujuan +
+                "\nKelas :" + hasil + "\nUsia" + Kereta + NmK);
     }
 }
---
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/formulir/R.java
@@ -0,0 +1,7 @@
+package id.sch.smktelkom_mlg.learn.formulir;
+
+/**
+ * Created by Windows 8.1 on 11/09/2016.
+ */
+public class R {
+}
